20890ba765611cc38beee7ecde5aeebdddd250bd
Udemy100 <srk03041@naver.com>
2024-06-07 22:21:49 +0900

    메인페이지 아이템 나열, 검색 기능

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,22 +1,40 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useState } from "react";
 import ProductCard from "../component/ProductCard";
 import { Row, Col, Container } from "react-bootstrap";
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { productActions } from "../action/productAction";
 import { commonUiActions } from "../action/commonUiAction";
 
+
 const ProductAll = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const error = useSelector((state) => state.product.error);
-  // 처음 로딩하면 상품리스트 불러오기
+  const products = useSelector((state)=>state.product.productList);
+  const [query] = useSearchParams();
+  const [searchQuery, setSearchQuery] = useState({
+    name: query.get("name") || "",
+  });
+
+  useEffect(() => {
+    setSearchQuery({
+      name: query.get("name") || "",
+    });
+  }, [query]);
+
+  useEffect(() => {
+    dispatch(productActions.getProductList({ ...searchQuery }));
+  }, [searchQuery]);
 
   return (
     <Container>
       <Row>
-        <Col md={3} sm={12}>
-          <ProductCard />
-        </Col>
+        {products.map((product) => (
+          <Col key={product.id} md={3} sm={12}>
+            <ProductCard product={product} />
+          </Col>
+        ))}
       </Row>
     </Container>
   );
